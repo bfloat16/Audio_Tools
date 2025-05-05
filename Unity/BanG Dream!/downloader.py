7e656cdf687a839d950bc07a007d99190bfa9b5f
bfloat16 <a1004428886@126.com>
2025-05-05 14:06:15 +0800

    update

--- Unity/BanG Dream!/downloader.py
+++ Unity/BanG Dream!/downloader.py
@@ -1,25 +1,13 @@
 import os
-import sys
-import json
 import time
 import requests
 import argparse
 from Crypto.Cipher import AES
 from concurrent import futures
-from tools import application_info_pb2
 from datetime import datetime, timezone
+from tools import application_info_pb2, assetbundle_info_pb2
+from google.protobuf.json_format import MessageToDict
 from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn, TimeElapsedColumn, TimeRemainingColumn
-
-from pythonnet import load
-load("coreclr")
-import clr
-script_dir = os.path.dirname(os.path.realpath(__file__))
-tools_dir = os.path.join(script_dir, "tools")
-sys.path.append(tools_dir)
-
-dll_path = os.path.join(tools_dir, "BanG_Dream_protobufnet.dll")
-clr.AddReference(dll_path)
-from BanG_Dream_protobufnet import ManifestLoader #别删，这玩意是从BanG_Dream_protobufnet.dll里导入的
 
 def args_parser():
     parser = argparse.ArgumentParser()
@@ -27,8 +15,8 @@ def args_parser():
     parser.add_argument("--threads", default=32)
     return parser.parse_args()
 
-BASE_URL = "https://api.garupa.jp/api/application"
 APP_HASH = "e15bca7f8e1c11ad1284a3ac1f9863b3a513c921c6f8a57a8fbd153c7539055c"
+APP_URL = "https://api.garupa.jp/api/application"
 ASSET_URL = "https://content.garupa.jp/Release"
 
 PROXIES= {
@@ -37,6 +25,25 @@ PROXIES= {
 }
 
 USER_AGENT = "UnityPlayer/2021.3.39f1 (UnityWebRequest/1.0, libcurl/8.5.0-DEV)"
+
+HEADERS1 = {
+    "Host":             "api.garupa.jp",
+    "User-Agent":       USER_AGENT,
+    "Accept-Encoding":  "deflate, gzip",
+    "Content-Type":     "application/octet-stream",
+    "Accept":           "application/octet-stream",
+    "X-ClientVersion":  "9.1.0",
+    "X-Signature":      "3cde36c1-b431-4458-90cf-469cb0096e0a",
+    "X-ClientPlatform": "Android",
+}
+
+HEADERS2 = {
+    "Host":             "content.garupa.jp",
+    "User-Agent":       USER_AGENT,
+    "Accept-Encoding":  "deflate, gzip",
+    "X-ClientPlatform": "Android",
+    "X-Unity-Version":  "2021.3.39f1",
+}
 
 columns = (SpinnerColumn(), TextColumn("[bold blue]{task.description}"), BarColumn(bar_width=100), "[progress.percentage]{task.percentage:>6.2f}%", TextColumn("{task.completed}/{task.total}"), TimeElapsedColumn(), "•", TimeRemainingColumn())
 
@@ -49,25 +56,7 @@ def get_asset_url_dec(data):
     return padded[:-pad_len]
 
 def get_asset_url():
-    headers1 = {
-        "Host":             "api.garupa.jp",
-        "User-Agent":       USER_AGENT,
-        "Accept-Encoding":  "deflate, gzip",
-        "Content-Type":     "application/octet-stream",
-        "Accept":           "application/octet-stream",
-        "X-ClientVersion":  "9.1.0",
-        "X-Signature":      "3cde36c1-b431-4458-90cf-469cb0096e0a",
-        "X-ClientPlatform": "Android",
-    }
-
-    headers2 = {
-        "Host":             "content.garupa.jp",
-        "User-Agent":       USER_AGENT,
-        "Accept-Encoding":  "deflate, gzip",
-        "X-ClientPlatform": "Android",
-        "X-Unity-Version":  "2021.3.39f1",
-    }
-    resp = requests.get(BASE_URL, headers=headers1, proxies=PROXIES)
+    resp = requests.get(APP_URL, headers=HEADERS1, proxies=PROXIES)
     resp.raise_for_status()
     app_info = application_info_pb2.AppGetResponse()
     resp = get_asset_url_dec(resp.content)
@@ -75,29 +64,36 @@ def get_asset_url():
     app_version = app_info.dataVersion
 
     timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S")
-    url = f"{ASSET_URL}/{app_version}_{APP_HASH}/Android/AssetBundleInfo?t={timestamp}"
+    url_meta = f"{ASSET_URL}/{app_version}_{APP_HASH}/Android/AssetBundleInfo?t={timestamp}"
     url_ab = f"{ASSET_URL}/{app_version}_{APP_HASH}/Android"
-    resp = requests.get(url, headers=headers2, proxies=PROXIES)
+    resp = requests.get(url_meta, headers=HEADERS2, proxies=PROXIES)
     resp.raise_for_status()
 
-    info = ManifestLoader.LoadBytesAndSerialize(resp.content)
-    info = json.loads(info)
-    return info, url_ab
+    info = assetbundle_info_pb2.AssetBundleInfo()
+    info.ParseFromString(resp.content)
+    bundle = MessageToDict(info)
+    return bundle, url_ab
 
-def bytes_to_gib(n_bytes):
-    return n_bytes / (1024 ** 3)
+def handler_assetbundle_info(root, data):
+    bundles = []
+    total_filesize = 0
+    filters = ["scenario", "story", "sound"]
 
-def calc_total_size(bundles):
-    return sum(int(b.get("fileSize", 0)) for b in bundles.values())
+    with Progress(*columns, transient=True) as progress:
+        task_id = progress.add_task("Checking", total=None)
+        for item in data["Bundles"].values():
+            name = item["BundleName"] + ".unity3d"
+            if not any(name.startswith(f) for f in filters):
+                continue
+            total_filesize += int(item["FileSize"])
+            bundles.append(name)
 
-def download_once(session, url, dest_path):
-    resp = session.get(url, timeout=30)
-    resp.raise_for_status()
-    with open(dest_path, 'wb') as f:
-        f.write(resp.content)
+            progress.update(task_id, advance=1)
+        total_filesize = total_filesize / (1024 ** 3)
+    return bundles, total_filesize
 
 def worker(bundle_name, base_url, out_root):
-    dest_path = os.path.join(out_root, f"{bundle_name}.unity3d")
+    dest_path = os.path.join(out_root, bundle_name)
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     session = requests.Session()
@@ -106,35 +102,29 @@ def worker(bundle_name, base_url, out_root):
 
     while True:
         timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S")
-        url = f"{base_url}/{bundle_name}?t={timestamp}"
+        url = f"{base_url}/{bundle_name.replace(".unity3d", "")}?t={timestamp}"
         try:
-            download_once(session, url, dest_path)
-            return
+            resp = session.get(url)
+            resp.raise_for_status()
+            with open(dest_path, 'wb') as f:
+                f.write(resp.content)
+                return
         except Exception as exc:
-            print(f"[W] Download failed for {bundle_name}: {exc}")
+            print(f"[Warning] {bundle_name}")
             time.sleep(1)
 
 if __name__ == "__main__":
     args = args_parser()
-    meta, url = get_asset_url()
+    bundle_info_dict, url_ab = get_asset_url()
+    bundles, total_filesize = handler_assetbundle_info(args.output_dir, bundle_info_dict)
 
-    bundles = meta["bundles"]
+    print(f"Total payload: {total_filesize:.2f} GiB ({len(bundles):,} files)")
 
-    total_bytes = calc_total_size(bundles)
-    print(f"Total payload: {bytes_to_gib(total_bytes):.2f} GiB ({total_bytes:,} bytes)")
-
-    out_root = args.output_dir
-    os.makedirs(out_root, exist_ok=True)
-
-    bundle_names = list(bundles.keys())
-    num_bundles = len(bundle_names)
+    os.makedirs(args.output_dir, exist_ok=True)
 
     with Progress(*columns) as progress:
-        task_id = progress.add_task("Downloading", total=num_bundles)
-
-        def wrapped_worker(name):
-            worker(name, url, out_root)
-            progress.update(task_id, advance=1)
-
+        task_id = progress.add_task("Downloading", total=len(bundles))
         with futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
-            executor.map(wrapped_worker, bundle_names)
+            future_list = [executor.submit(worker, name, url_ab, args.output_dir) for name in bundles]
+            for _ in futures.as_completed(future_list):
+                progress.update(task_id, advance=1)
--- Unity/BanG Dream!/tools/application_info_pb2.py
+++ Unity/BanG Dream!/tools/application_info_pb2.py
@@ -14,13 +14,13 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x61pplication_info.proto\"\xd9\x01\n\x0e\x41ppGetResponse\x12\x15\n\rclientVersion\x18\x01 \x01(\t\x12\x13\n\x0b\x64\x61taVersion\x18\x02 \x01(\t\x12\x11\n\tappStatus\x18\x03 \x01(\t\x12\x14\n\x0c\x63lientStatus\x18\x04 \x01(\t\x12\x0e\n\x06schema\x18\x05 \x01(\t\x12\r\n\x05gacha\x18\x07 \x01(\t\x12\x11\n\tmultiLive\x18\x08 \x01(\t\x12\x10\n\x08starShop\x18\t \x01(\t\x12\x19\n\x11masterDataVersion\x18\n \x01(\t\x12\x13\n\x0bphotonAppId\x18\x0b \x01(\t')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x61pplication_info.proto\x12\x10\x61pplication_info\"\xdf\x01\n\x0e\x41ppGetResponse\x12\x15\n\rclientVersion\x18\x01 \x01(\t\x12\x13\n\x0b\x64\x61taVersion\x18\x02 \x01(\t\x12\x11\n\tappStatus\x18\x03 \x01(\t\x12\x14\n\x0c\x63lientStatus\x18\x04 \x01(\t\x12\x0e\n\x06schema\x18\x05 \x01(\t\x12\r\n\x05gacha\x18\x07 \x01(\t\x12\x11\n\tmultiLive\x18\x08 \x01(\t\x12\x10\n\x08starShop\x18\t \x01(\t\x12\x19\n\x11masterDataVersion\x18\n \x01(\t\x12\x13\n\x0bphotonAppId\x18\x0b \x01(\tJ\x04\x08\x06\x10\x07\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'application_info_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
-  _globals['_APPGETRESPONSE']._serialized_start=27
-  _globals['_APPGETRESPONSE']._serialized_end=244
+  _globals['_APPGETRESPONSE']._serialized_start=45
+  _globals['_APPGETRESPONSE']._serialized_end=268
 # @@protoc_insertion_point(module_scope)
